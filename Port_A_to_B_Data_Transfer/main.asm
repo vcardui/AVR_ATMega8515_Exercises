51687de2ccd0109ec6396c907b4188a800c530a4
Vanessa Reteguín <145515264+vcardui@users.noreply.github.com>
2025-05-19 18:27:01 -0600

    Create main.asm

---
+++ Port_A_to_B_Data_Transfer/main.asm
@@ -0,0 +1,12 @@
+// +----------------------------------------------------------------------------+
+// | Author.......: Vanessa Reteguín <vanessa@reteguin.com>
+// | Author.......: 
+// | First release: May 19th, 2025
+// | Last update..: May 19th, 2025
+// | WhatIs.......: Port A to B Data Transfer - Main
+// +----------------------------------------------------------------------------+
+
+// ------------ Resources / Documentation involved -------------
+// Bit Shifting with Delays Teacher's Tutorial: https://www.youtube.com/watch?v=apbuP5SLwVA
+
+.INCLUDE <M8515DEF.INC>
