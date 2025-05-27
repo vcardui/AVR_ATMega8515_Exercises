208626f39cf0996526b07e3407d1d5720770a610
Vanessa Reteguín <145515264+vcardui@users.noreply.github.com>
2025-05-27 10:51:06 -0600

    Update main.asm

--- 3_7SegmentsDisplay_Cycle_0_to_F/main.asm
+++ 3_7SegmentsDisplay_Cycle_0_to_F/main.asm
@@ -1,8 +1,7 @@
 // +----------------------------------------------------------------------------+
 // | Author.......: Vanessa Reteguín <vanessa@reteguin.com>
-// | Author.......: 
 // | First release: May 19th, 2025
-// | Last update..: May 26rd, 2025
+// | Last update..: May 27rd, 2025
 // | WhatIs.......: 7 Segments Display Cycle: 0 to F - Main
 // +----------------------------------------------------------------------------+
 
@@ -13,6 +12,7 @@
 // ------------ Resources / Documentation involved -------------
 // Bit Shifting with Delays Teacher's Tutorial: https://www.youtube.com/watch?v=apbuP5SLwVA
 // AVR Stack Pointer y Subrutinas: https://youtu.be/489lpgMzDzk?si=Z6iZih3l-I8lIOES
+// "macros" and "lookup tables: https://www.instructables.com/Command-Line-Assembly-Language-Programming-for-Ard-3/
 
 // AVR Delay Loop Calculator: http://darcy.rsgc.on.ca/ACES/TEI4M/AVRdelay.html
 ; Assembly code auto-generated
@@ -40,54 +40,76 @@ L1:		dec  r27
 
 			LDI		R16,0XFF	// Llenamos el registro 16 con puros 1s
 			OUT		DDRA,R16	// Configuramos el puerto A como salida
-
-							  // Display -> Binario -> Hex
-			LDI		R17,0B0111111	// 0 -> 0111111 -> 3F
-			LDI		R18,0B0000110	// 1 -> 0000110 -> 06
-			LDI		R19,0B1011011	// 2 -> 1011011 -> 5B
-			LDI		R20,0B1001111	// 3 -> 1001111 -> 4F
-			LDI		R21,0B1100110	// 4 -> 1100110 -> 66
-			LDI		R22,0B1101101	// 5 -> 1101101 -> 6D
-			LDI		R23,0B1111101	// 6 -> 1111101 -> 7D
-			LDI		R24,0B0000111	// 7 -> 0000111 -> 07
-			LDI		R25,0B1111111	// 8 -> 1111111 -> 7F
-			LDI		R26,0B1101111	// 9 -> 1101111 -> 6F
-			LDI		R27,0B1110111	// A -> 1110111 -> 77
-			LDI		R28,0B1111100	// b -> 1111100 -> 7C
-			LDI		R29,0B0111001	// C -> 0111001 -> 39
-			// LDI		R30,0B1011110	// d -> 1011110 -> 5E
-			// LDI		R31,0B1111001	// E -> 1111001 -> 79
-			// LDI		R32,0B1110001	// F -> 1110001 -> 71
 			
-CICLO:		OUT		PORTA,R17
+CICLO:		
+			LDI		R17,0B0111111	// 0 -> 0111111 -> 3F
+			OUT		PORTA,R17
 			RCALL	SUBRRUTINA
-			OUT		PORTA,R18
+
+			LDI		R17,0B0000110	// 1 -> 0000110 -> 06
+			OUT		PORTA,R17
 			RCALL	SUBRRUTINA
-			OUT		PORTA,R19
+
+			LDI		R17,0B1011011	// 2 -> 1011011 -> 5B
+			OUT		PORTA,R17
 			RCALL	SUBRRUTINA
-			OUT		PORTA,R20
+
+			LDI		R17,0B1001111	// 3 -> 1001111 -> 4F
+			OUT		PORTA,R17
 			RCALL	SUBRRUTINA
-			OUT		PORTA,R21
+
+			LDI		R17,0B1100110	// 4 -> 1100110 -> 66
+			OUT		PORTA,R17
 			RCALL	SUBRRUTINA
-			OUT		PORTA,R22
+
+			LDI		R17,0B1101101	// 5 -> 1101101 -> 6D
+			OUT		PORTA,R17
 			RCALL	SUBRRUTINA
-			OUT		PORTA,R23
+
+			LDI		R17,0B1111101	// 6 -> 1111101 -> 7D
+			OUT		PORTA,R17
 			RCALL	SUBRRUTINA
-			OUT		PORTA,R24
+
+			LDI		R17,0B0000111	// 7 -> 0000111 -> 07
+			OUT		PORTA,R17
 			RCALL	SUBRRUTINA
-			OUT		PORTA,R25
+
+			LDI		R17,0B1111111	// 8 -> 1111111 -> 7F
+			OUT		PORTA,R17
 			RCALL	SUBRRUTINA
-			OUT		PORTA,R26
+
+			LDI		R17,0B1101111	// 9 -> 1101111 -> 6F
+			OUT		PORTA,R17
 			RCALL	SUBRRUTINA
-			OUT		PORTA,R27
+
+			LDI		R17,0B1110111	// A -> 1110111 -> 77
+			OUT		PORTA,R17
 			RCALL	SUBRRUTINA
-			OUT		PORTA,R28
+
+			LDI		R17,0B1111100	// b -> 1111100 -> 7C
+			OUT		PORTA,R17
+			RCALL	SUBRRUTINA
+
+			LDI		R17,0B0111001	// C -> 0111001 -> 39
+			OUT		PORTA,R17
+			RCALL	SUBRRUTINA
+
+			LDI		R17,0B1011110	// d -> 1011110 -> 5E
+			OUT		PORTA,R17
+			RCALL	SUBRRUTINA
+			
+			LDI		R17,0B1111001	// E -> 1111001 -> 79
+			OUT		PORTA,R17
+			RCALL	SUBRRUTINA
+
+			LDI		R17,0B1110001	// F -> 1110001 -> 71
+			OUT		PORTA,R17
 			RCALL	SUBRRUTINA
 			
 			RJMP	CICLO
 
 SUBRRUTINA:
-			LDI  R29, 10
+			LDI  R29, 5
 			LDI  R30, 150
 			LDI  R31, 128
 	L1:		DEC  R31
